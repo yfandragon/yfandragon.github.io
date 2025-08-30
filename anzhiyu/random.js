8cc68d7607b9cd153737eeda9cb9282f92f4b574
yfandragon <duanyf20001007@163.com>
2025-08-30 18:05:46 +0800

    Site updated: 2025-08-30 18:05:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/28/hello-world/","2025/08/28/test/","2025/08/30/Neovim/"];function toRandomPost(){
+var posts=["2025/08/28/test/","2025/08/30/这是一个新blog/","2025/08/28/hello-world/","2025/08/30/Neovim/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
